b30c48087709d99b891ff983b3f43279426a5b51
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-23 17:57:39 -0400

    Diseño de calculadora listo

--- src/presentation/CalculatorScreen.tsx
+++ src/presentation/CalculatorScreen.tsx
@@ -10,8 +10,12 @@ export const CalculatorScreen = () => {
   return (
     <View style={ styles.calculatorContainer }>
 
-      <Text style={ styles.subResult }>1,500.00</Text>
-      <Text style={ styles.mainResult }>1,500.00</Text>
+      <View style={{ paddingHorizontal: 30}}>
+        <Text style={ styles.subResult }>1,500.00</Text>
+        <Text style={ styles.mainResult }>1,500.00</Text>
+
+      </View>
+
       
 
 
@@ -26,8 +30,44 @@ export const CalculatorScreen = () => {
         <CalculatorButton blackText label="del" color={ lightGray } onPress={ () => console.log('C') } />
         <CalculatorButton label="/" color={ orange } onPress={ () => console.log('C') } />
     
-
       </View>
+
+      <View style={ styles.row }>
+
+        <CalculatorButton label="7" onPress={ () => console.log('C') } />
+        <CalculatorButton label="8" onPress={ () => console.log('C') } />
+        <CalculatorButton label="9" onPress={ () => console.log('C') } />
+        <CalculatorButton label="x" color={ orange } onPress={ () => console.log('C') } />
+    
+      </View>
+
+      <View style={ styles.row }>
+
+        <CalculatorButton label="4" onPress={ () => console.log('C') } />
+        <CalculatorButton label="5" onPress={ () => console.log('C') } />
+        <CalculatorButton label="6" onPress={ () => console.log('C') } />
+        <CalculatorButton label="-" color={ orange } onPress={ () => console.log('C') } />
+    
+      </View>
+
+      <View style={ styles.row }>
+
+        <CalculatorButton label="1" onPress={ () => console.log('C') } />
+        <CalculatorButton label="2" onPress={ () => console.log('C') } />
+        <CalculatorButton label="3" onPress={ () => console.log('C') } />
+        <CalculatorButton label="+" color={ orange } onPress={ () => console.log('C') } />
+    
+      </View>
+
+      <View style={ styles.row }>
+
+        <CalculatorButton doubleSize label="0" onPress={ () => console.log('C') } />
+        <CalculatorButton label="." onPress={ () => console.log('C') } />
+        <CalculatorButton label="=" color={ orange } onPress={ () => console.log('C') } />
+    
+      </View>
+
+
 
 
     </View>
