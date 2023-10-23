652a7b5fc89bd2484754be09874653ff9c174740
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-23 17:54:53 -0400

    Boton de calculadora agregado

--- src/presentation/components/CalculatorButton.tsx
+++ src/presentation/components/CalculatorButton.tsx
@@ -4,34 +4,37 @@ import { colors, styles } from '../theme/app-theme';
 
 
 interface Props {
-    label:string;
-    color?: string;
-    doubleSize?: boolean;
-    blackText?: boolean;
-    onPress: ( numeroTexto: string ) => void;
+  label: string;
+  color?: string;
+  doubleSize?: boolean;
+  blackText?: boolean;
+  onPress: ( numeroTexto: string ) => void;
 }
 
-export const CalculatorButton = ({ 
-  label, 
-  color = colors.darkGray, 
-  doubleSize = false, 
+export const CalculatorButton = ( {
+  label,
+  color = colors.darkGray,
+  doubleSize = false,
   blackText = false,
-  onPress 
-}: Props) => {
-    return (
-        <Pressable
-            onPress={ () => onPress( label ) }
-        >
-            <View style={{ 
-                ...styles.button ,
-                backgroundColor: color,
-                width: ( doubleSize ) ? 180 : 80
-            }}>
-                <Text style={{ 
-                    ...styles.buttonText,
-                    color: ( blackText ) ? 'black' : 'white'
-                }}>{ label }</Text>
-            </View>
-        </Pressable>
-    )
-}
+  onPress
+}: Props ) => {
+  return (
+    <Pressable
+      onPress={ () => onPress( label ) }
+      style={ ( { pressed } ) => ( {
+        ...styles.button,
+        backgroundColor: color,
+        width: ( doubleSize ) ? 180 : 80,
+        opacity: ( pressed ) ? 0.8 : 1,
+      } )
+      }
+    >
+
+      <Text style={ {
+        ...styles.buttonText,
+        color: ( blackText ) ? 'black' : 'white'
+      } }>{ label }</Text>
+
+    </Pressable>
+  );
+};
